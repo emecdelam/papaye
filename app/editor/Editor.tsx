a7e1d181f186291ede1676d4a57ffd895780e7d9
Kaporos <theo@daron.be>
2023-06-26 13:11:13 +0200

    basic editor features to test them out !

--- app/editor/Editor.tsx
+++ app/editor/Editor.tsx
@@ -1,25 +1,57 @@
 
-import { useState } from "react"
-import { createEditor } from "slate"
+import { useCallback, useState } from "react"
+import { Transforms, createEditor, Editor, Element } from "slate"
 import {withReact, Slate, Editable} from "slate-react"
 
 
-const initialValue: CustomElement[] = [
+const initialValue: Descendant[] = [
     {
         type: 'paragraph',
         children: [{text: "Hey im a super paragraph"}]
     }
 ]
 
-export default function Editor() {
+const CodeElement = props => {
+    return (
+        <pre {...props.attributes}>
+            <code>{props.children}</code>
+        </pre>
+    )
+}
+
+const DefaultElement = props => {
+    return <p {...props.attributes}>{props.children}</p>
+}
+
+export default function EditorComponent() {
     const [editor] = useState(() => withReact(createEditor()))
-    
+    const renderElement = useCallback(props => {
+        switch (props.element.type) {
+            case 'code':
+                return <CodeElement {...props}/>
+            default:
+                return <DefaultElement {...props}/>
+        }
+    }, [])
 
     return (<>
     
         <p>Hey im the editor</p>
         <Slate editor={editor} initialValue={initialValue}>
-            <Editable/>
+            <Editable renderElement={renderElement} onKeyDown={event => {
+                console.log(event.key)
+                if (event.key == "µ" && event.ctrlKey) {
+                    event.preventDefault() 
+                    const [match] = Editor.nodes(editor, {
+                        match: n => n.type === "code"
+                    })
+                    Transforms.setNodes(
+                        editor,
+                        {type: match ? 'paragraph' : 'code'},
+                        {match: n => Element.isElement(n) && Editor.isBlock(editor, n)}
+                    )
+                }
+            }}/>
         </Slate>
     
     </>)
@@ -30,13 +62,14 @@ export default function Editor() {
 import { BaseEditor, Descendant } from 'slate'
 import { ReactEditor } from 'slate-react'
 
-type CustomElement = { type: 'paragraph', children: CustomText[] }
-type CustomText = { text: string }
+type CustomElement = { type: 'paragraph' | 'code', children: CustomText[] }
+type CustomText = { text: string}
 
 declare module 'slate' {
   interface CustomTypes {
     Editor: BaseEditor & ReactEditor
     Element: CustomElement
     Text: CustomText
+    Node: CustomElement | CustomText
   }
 }
