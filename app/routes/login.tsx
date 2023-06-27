d4453662fffe316d89294990a8f65a1ac7444d4b
Kaporos <theo@daron.be>
2023-06-27 13:21:18 +0200

    💄 New login page !

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,7 +1,10 @@
-import { Input } from "@/components/ui/input";
+import FormElement from "@/components/ui/forms/control";
+import { Input } from "@/components/ui/forms/input";
+import { Label } from "@/components/ui/forms/label";
+import SocialButton from "@/components/ui/forms/social";
 import { ActionArgs, LoaderArgs, V2_MetaFunction, json } from "@remix-run/node";
 import { Form, Link, useLoaderData } from "@remix-run/react";
-import { authenticator, getUser } from "~/services/auth.server";
+import { authenticator } from "~/services/auth.server";
 import {  destroySession, getSession } from "~/services/session.server";
 
 export const meta: V2_MetaFunction = () => {
@@ -15,28 +18,44 @@ export const meta: V2_MetaFunction = () => {
 export default function LoginPage() {
     const data = useLoaderData<typeof loader>();
     return <div className="h-full">
-        <div className="w-full h-full grid place-items-center"  style={{backgroundImage: "url('https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80')", backgroundSize: "cover"}}>
-            <Form method="post" className="w-1/4 bg-white rounded-md p-10 shadow">
-                <div className="mb-10 font-bold text-3xl">
-                <p>Happy to see you back !</p>
+        <div className="w-full h-full grid place-items-center"  style={{backgroundImage: "url('https://images.pexels.com/photos/1064162/pexels-photo-1064162.jpeg?cs=srgb&dl=pexels-eberhard-grossgasteiger-1064162.jpg&fm=jpg')", backgroundSize: "cover"}}>
+            <div className="w-5/12 h-1/2 bg-white rounded-md p-10 shadow">
+                <div className="mb-10 font-bold text-3xl w-full text-center">
+                  <p>Happy to see you back !</p>
                 </div>
-                <div className="mb-5">
-                    <label className="block text-md font-semibold" htmlFor="email">Email</label>
-                    <Input type="email" placeholder="john@doe.com"></Input>
+
+                <div className="flex justify-between w-full">
+                  <div className="text-center w-5/12 ">       
+                  
+                  <Form method="post">
+                    <FormElement>
+                        <Label htmlFor="email">Email</Label>
+                        <Input id="email" name="email" type="email" placeholder="john@doe.com"/>
+                    </FormElement>
+    
+                    <FormElement>
+                        <Label htmlFor="password">Mot de passe</Label>
+                        <Input id="password" name="email" type="password" placeholder="i like potatoes"/>
+                    </FormElement>
+                    
+                    
+                    {data.error ? <div className="bg-red-300 p-2 rounded-md">{data.error.message}</div> : ""}
+                    
+                    <button className=" bg-blue-700 text-white rounded-md px-8 h-10 cursor-pointer mt-8 mb-3" type="submit">Me connecter</button>
+                    <br />
+                    <Link to="/signup" className="underline">Je n'ai pas encore de compte</Link>
+                    </Form>
+                  </div>
+  
+                  <div className="w-1/2 border-l-2 px-10">
+                    <SocialButton name="Github"/>
+                    <SocialButton name="Google"/>
+                    <SocialButton name="Microsoft"/>  
+                  </div>
                 </div>
-                <div className="mb-10">
-                    <label className="block text-md font-semibold" htmlFor="password">Password </label>
-                    <Input type="password" placeholder="toto1234"></Input>
-                </div>
-                
-                
-                {data.error ? <div className="h-full bg-red-300 p-3 rounded-md mb-10">{data.error.message}</div> : ""}
-                
-                <div className="w-full flex justify-between items-center ">
-                <button className="bg-blue-700 text-white rounded-md px-6 h-10 cursor-pointer " type="submit">Log in</button>
-                <Link to="/signup" className="underline">I want to create an account.</Link>
-                </div>
-            </Form>
+      
+         
+            </div>
         </div>
 
  
